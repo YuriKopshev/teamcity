5ad78e0ac9313d4dfdc6be952aee5bd591c307a5
Yuri Kopshev <yura.kopshev@gmail.com>
2026-04-11 21:14:25 +0200

    changed build.kt

--- .teamcity/buildTypes/Build.kt
+++ .teamcity/buildTypes/Build.kt
@@ -1,6 +1,6 @@
 package _Self.buildTypes
 
-import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.v2024_03.*  
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
